981063760a8e08ece4a461f5a68df87842f720a6
David Jones <david@weaponsdivision.co.uk>
2016-03-08 19:06:24 +0000

    Removed space between code blocks causing problems setting headers

--- core/database/connect.php
+++ core/database/connect.php
@@ -2,9 +2,7 @@
 /*$connect_error = "Site under maintenance.";
 mysql_connect('localhost', 'webdevu1_webley', 'supplier123', '') or die($connect_error);
 mysql_select_db('webdevu1_useru') or die($connect_error);*/
-?>
 
-<?php
 $connect_error = "Site under maintenance.";
 mysql_connect(getenv("USERU_DB_HOST"), getenv("USERU_DB_USER"), getenv("USERU_DB_PASSWD"), '') or die($connect_error);
 mysql_select_db(getenv("USERU_DB_NAME")) or die($connect_error);
